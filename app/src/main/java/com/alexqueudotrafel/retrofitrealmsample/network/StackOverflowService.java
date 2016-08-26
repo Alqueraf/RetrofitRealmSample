2227b08a96ec52c82d43c53db8d24309c754a9dc
Alex Queudot Rafel <aqueudot@signsoft.com>
2016-08-26 16:04:43 +0200

    Minor updates

--- app/src/main/java/com/alexqueudotrafel/retrofitrealmsample/network/StackOverflowService.java
+++ app/src/main/java/com/alexqueudotrafel/retrofitrealmsample/network/StackOverflowService.java
@@ -3,13 +3,13 @@ package com.alexqueudotrafel.retrofitrealmsample.network;
 import com.alexqueudotrafel.retrofitrealmsample.model.Question;
 import com.alexqueudotrafel.retrofitrealmsample.network.helper.MyGsonConverterFactory;
 
-
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+
 
 /**
  * Created by alexqueudotrafel on 16/03/16.
